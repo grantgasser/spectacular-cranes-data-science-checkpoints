7acb89a672f406be4f563bdd1c0c317ecbef7b35
Grant Gasser <glgasser@gmail.com>
2019-10-13 20:16:31 -0500

    Update cp1_2.sql

--- checkpoint-1/src/cp1_2.sql
+++ checkpoint-1/src/cp1_2.sql
@@ -3,7 +3,8 @@
 -- Time permitting: What categories of allegations are changed the most?
 
 
-select * from data_allegation da, case_info_08282018 ca, data_officerallegation doa, data_allegationcategory dac
+select current_category as old_category_desc, current_category_type as old_category, category as current_category from data_allegation da, case_info_08282018 ca, data_officerallegation doa, data_allegationcategory dac
 where da.crid = ca.log_no
 and doa.allegation_id = da.id
 and dac.id = doa.allegation_category_id
+and dac.category != ca.current_category
