894113756fea4f4e64cdfa5a6f230a45268ef486
Grant Gasser <glgasser@gmail.com>
2019-10-13 21:01:24 -0500

    Update cp1_3.sql

--- checkpoint-1/src/cp1_3.sql
+++ checkpoint-1/src/cp1_3.sql
@@ -3,12 +3,18 @@
 -- of allegation (e.g. “Sexual assault” -> “Illegal Search and Seizure”)
 
 
+-- TIME PERMITTING: normalize these numbers, allows a better comparison
+
 -- see allegation category distribution from old data
 select current_category, count(current_category) 
 from case_info_08282018 group by current_category
 
 
 -- category distribution from new cpdb data
-
+select category, count(category) from data_allegation da, 
+data_officerallegation doa, data_allegationcategory dac
+where doa.allegation_id = da.id
+and dac.id = doa.allegation_category_id
+group by category
 
 
