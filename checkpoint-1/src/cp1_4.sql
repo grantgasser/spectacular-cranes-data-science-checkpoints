c1e3ecd18131ab16cfa25d8880b51221c5d6020b
Grant Gasser <glgasser@gmail.com>
2019-10-13 21:34:47 -0500

    Update cp1_4.sql

--- checkpoint-1/src/cp1_4.sql
+++ checkpoint-1/src/cp1_4.sql
@@ -1,6 +1,6 @@
 -- Checkpoint 1, Question 4
--- What data attributes are changed the most?
--- (e.g. category of the allegation, or officers involved, etc)
+-- Using the data generated in Question 3, 
+-- can we use the discipline data to see how many allegations were disciplined, grouped by category.
 
 select count(CASE WHEN disciplined THEN 1 END) as Disciplined_count, Count(*) as total_count, category 
 from data_allegation da, data_officerallegation doa, data_allegationcategory dac
