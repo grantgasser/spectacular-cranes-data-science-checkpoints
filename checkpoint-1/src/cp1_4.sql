f0d70fa3d46db00c399c039d29cf8f3ccb802537
Grant Gasser <glgasser@gmail.com>
2019-10-13 21:33:30 -0500

    Update cp1_4.sql

--- checkpoint-1/src/cp1_4.sql
+++ checkpoint-1/src/cp1_4.sql
@@ -1,3 +1,9 @@
 -- Checkpoint 1, Question 4
 -- What data attributes are changed the most?
 -- (e.g. category of the allegation, or officers involved, etc)
+
+select count(CASE WHEN disciplined THEN 1 END) as Disciplined_count, Count(*) as total_count, category 
+from data_allegation da, data_officerallegation doa, data_allegationcategory dac
+where da.id = doa.allegation_id
+and dac .id = doa.allegation_category_id group by category
+
