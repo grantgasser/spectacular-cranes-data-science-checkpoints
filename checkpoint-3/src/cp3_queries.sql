205b174e7bc02c6475ab0865ad7e2ce42ba16925
Grant Gasser <glgasser@gmail.com>
2019-11-18 18:05:02 -0600

    Update cp3_queries.sql

--- checkpoint-3/src/cp3_queries.sql
+++ checkpoint-3/src/cp3_queries.sql
@@ -8,3 +8,18 @@ where allegation_mapping_boolean is false) / (select count(*) as total
 from allegation_mapping_old_and_new)::float * 100
 
 -- 56356 / 175533 = 32%
+
+-- Q3 pt2
+select statute_description, count(*) as cnt from allegation_mapping_old_and_new as map_old_new,
+temp_arrests_and_allegations_total aa
+where map_old_new.crid_and_log_no = aa.crid
+and map_old_new.allegation_mapping_boolean is false
+group by statute_description
+order by cnt desc
+limit 3
+
+/*
+"PCS - POSSESSION - LESS THAN 15 GRMS - COCAINE"	"17802"
+"DISORDERLY CONDUCT - BREACH OF PEACE"	"10026"
+"CRIMINAL TRESPASS TO STATE LAND"	"9447"
+*/
